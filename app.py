8ec2db1ef41acbd219920fc716e897c5e0dbc743
Bogida69 <adinathkulkarni86@gmail.com>
2025-02-11 20:16:30 +0530

    updated the ports

--- app.py
+++ app.py
@@ -185,5 +185,8 @@ def static_files(filename):
 
 if __name__ == '__main__':
    
-    port = int(os.getenv('PORT', 8000)) 
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False)
