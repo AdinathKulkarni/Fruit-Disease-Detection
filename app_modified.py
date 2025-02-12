376d6e32e6d9cab218e893af2ec5b2c1bc6c9cfa
Bogida69 <adinathkulkarni86@gmail.com>
2025-02-12 22:23:19 +0530

    Updtated the port number to 4000 for deployment

--- app_modified.py
+++ app_modified.py
@@ -180,4 +180,4 @@ def static_files(filename):
     return send_from_directory(OUTPUT_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=4000)
