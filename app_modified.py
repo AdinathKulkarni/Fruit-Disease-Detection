92aca68ad85138acd7628f1bb76c1e885d0bd186
Bogida69 <adinathkulkarni86@gmail.com>
2025-02-18 07:37:37 +0530

    Changed the port number to render default

--- app_modified.py
+++ app_modified.py
@@ -188,4 +188,4 @@ def static_files(filename):
     return send_from_directory(OUTPUT_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 10000))
